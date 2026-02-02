bc9e3819fb182f1646bfca7fc43357a918077b33
Collinsemmanuel <emmanuelkibet42018@gmail.com>
2026-02-02 11:00:09 +0300

    feat(02-02): add video component barrel exports
    - Export BookmarkButton and VideoBookmarks from index.ts
    - Following project barrel export patterns
    - Prepared for additional exports from 02-01 plan

---
+++ frontend/src/components/video/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Video Components
+ *
+ * Barrel export for video-related components.
+ */
+
+// Bookmark components
+export { default as BookmarkButton } from './BookmarkButton'
+export { default as VideoBookmarks } from './VideoBookmarks'
+
+// Note: InteractiveVideoPlayer, VideoChapters, and VideoControls
+// will be added by plan 02-01 when executed.
